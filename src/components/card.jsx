f785f15edc72f5ca0516f337ecc95fa8b6488996
davidnherran <davidnherran@gmail.com>
2020-06-14 16:50:18 -0500

    Tarjetas estaticas => componente reutilizable

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import exersice from '../images/exercise.png'
+import circles from '../images/circles.png'
 import './styles/Card.css'
 
 class Card extends React.Component {
     render(){
+        const { title, description, image, leftColor, rightColor } = this.props
         return (
-           <div className="card mx-auto Fitness-Card">
+           <div className="card mx-auto Fitness-Card"
+                style={{
+                    backgroundImage: `url(${circles}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+                }}
+           >
                <div className="body-card">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exersice} alt="" className="float-right" />
+                            <img src={image} alt="" className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calistthenics</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                </div>
--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card />, container)
+ReactDOM.render(<Card
+                  title="Technique Guides"
+                  description="Learn amazing street workout and calistthenics"
+                  image="https://raw.githubusercontent.com/davidnherran/fitness-manager/master/src/images/exercise.png"
+                  leftColor="#A74CF2"
+                  rightColor="#617BFB"
+                />, container)
