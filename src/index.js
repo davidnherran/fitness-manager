3b301b8591765259c962ec0a872cd4b983f5c5a8
davidnherran <davidnherran@gmail.com>
2020-06-14 15:49:03 -0500

    Creación de vista en tarjetas (cards) - integración framework bootstrap

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/card'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+const container = document.getElementById('root')
+
+ReactDOM.render(<Card />, container)
