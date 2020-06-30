96628e544a2a4f1073f7874bc78e47888e783194
David Niño Herrán <45274593+davidnherran@users.noreply.github.com>
2020-06-30 14:07:33 -0500

    Lifting State Up, estado del componente, levantado correctamente

--- src/pages/ExcerciseNew.jsx
+++ src/pages/ExcerciseNew.jsx
@@ -1,89 +1,39 @@
-import React from 'react'
+import React from "react";
+import ExcerciseForm from "../components/excerciseForm";
+import Card from "../components/card";
 
 class ExcerciseNew extends React.Component {
+  state = {
+    form: {
+        title: '', 
+        description: '', 
+        image: '', 
+        leftColor: '', 
+        rightColor: ''
+    },
+  };
 
-    state = {}
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-    handleSubmit = e => {
-        e.preventDefault()
-        console.log(this.state)
-    }
-
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-    render(){
-        return (
-            <div className="container">
-            <form 
-                onSubmit={this.handleSubmit}
-            >
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="title" 
-                        name="title"
-                        onChange={this.handleChange}
-                        value={this.state.title}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="description" 
-                        name="description"
-                        onChange={this.handleChange}
-                        value={this.state.description}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="img" 
-                        name="img"
-                        onChange={this.handleChange}
-                        value={this.state.img}
-                    />
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="leftColor" 
-                            name="leftColor"
-                            onChange={this.handleChange}
-                            value={this.state.leftColor}
-                        />
-                    </div>
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            placeholder="rightColor" 
-                            name="rightColor"
-                            onChange={this.handleChange}
-                            value={this.state.rightColor}
-                        />    
-                    </div>
-                </div>
-                
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Submit
-                </button>
-            </form>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm">
+          <Card {...this.state.form} />
         </div>
-        )
-    }
+        <div className="col-sm">
+          <ExcerciseForm onChange={this.handleChange} form={this.state.form} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ExcerciseNew
+export default ExcerciseNew;
